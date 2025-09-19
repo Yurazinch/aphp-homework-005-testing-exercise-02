21f62237cfd3302d51a4ec18100a2d546ac8b8ed
Yuriy Zinchenko <yurazinch@gmail.com>
2025-09-19 20:10:27 +0300

    first commit

---
+++ src/User.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+class User extends UserTableWrapper implements TableWrapperInterface
+{
+    public function insert(array $values): void
+    {
+        
+    }
+    public function update(int $id, array $values): array
+    {
+
+    }
+    public function delete(int $id): void
+    {
+
+    }
+    public function get(): array
+    {
+
+    }
+}   
---
+++ src/UserTableWrapper.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+class UserTableWrapper
+{
+    private array $rows;
+
+    /**
+     * @param array|[column => row_value] $values
+     */
+    
+    public function insert(array $values): void
+    {
+        $this->rows[] = $values;
+    }
+}
